60d0c079bd9ba4e4003a6b893d160c17bb5f936c
simofanna <simofanna93@gmail.com>
2018-02-19 10:59:15 +0100

    fix

--- player.py
+++ player.py
@@ -6,7 +6,6 @@ class Player:
         VERSION = "0.8"
         holeCards = game_state["players"][VERSION]["hole_cards"]
         communityCards = game_state["players"][VERSION]["community_cards"]
-
         try:
             if hole_cards[VERSION][0]["suit"] == hole_cards[VERSION][1]["suit"] or hole_cards[VERSION][0]["rank"] == hole_cards[VERSION][1]["rank"]:
                 return 25
@@ -14,6 +13,9 @@ class Player:
                 return 10
         except ValueError:
             print(community_cards)
+
+        finally: 
+            return 10
 
     def showdown(self, game_state):
         pass
