99403dd2a7a56789aecf19775c6a7ba40f800083
simofanna <simofanna93@gmail.com>
2018-02-19 10:37:38 +0100

    fix

--- player.py
+++ player.py
@@ -2,12 +2,7 @@
 class Player:
     VERSION = 0.8
 
-    def betRequest(self, game_state):
-            json_object = json.loads(game_state)
-            hole_cards = game_state['hole_cards']
-            print(hole_cards)
-          
-                
+    def betRequest(self, game_state):      
         return 20
 
     def showdown(self, game_state):
