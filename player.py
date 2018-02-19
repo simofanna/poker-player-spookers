07005fc2ed61d6516ab8a7741a1a9ea2e6ee8e8b
simofanna <simofanna93@gmail.com>
2018-02-19 10:35:17 +0100

    print

--- player.py
+++ player.py
@@ -1,9 +1,15 @@
 
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = 0.8
 
     def betRequest(self, game_state):
-        return 0
+            json_object = json.loads(game_state)
+            hole cards = game_state['hole_cards']
+            hole_cards = json_object['players'][9]['hole_cards'])
+            print(hole_cards)
+          
+                
+        return 20
 
     def showdown(self, game_state):
         pass
