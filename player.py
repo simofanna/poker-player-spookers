9cb895e797f76cdcec83a27ef3bda4058a038ecc
Balazs Pest <pest.balazs@gmail.com>
2018-02-19 11:41:31 +0100

    all-in

--- player.py
+++ player.py
@@ -2,13 +2,8 @@
 class Player:
     VERSION = 0.8
 
-    def betRequest(self, game_state):
-            json_object = json.loads(game_state)
-            hole_cards = game_state['hole_cards']
-            print(hole_cards)
-          
-                
-        return 20
+    def betRequest(self, game_state):     
+        return 1000
 
     def showdown(self, game_state):
         pass
