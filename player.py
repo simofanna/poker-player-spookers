3a430469591d13d594c232009a5d51de68f0c7bd
simofanna <simofanna93@gmail.com>
2018-02-19 11:30:24 +0100

    tr

--- player.py
+++ player.py
@@ -3,16 +3,18 @@ class Player:
     VERSION = "8"
 
     def betRequest(self, game_state): 
-        VERSION = 8
+        try:
+            
         holeCards = game_state["players"][VERSION]["hole_cards"]
         communityCards = game_state["players"][VERSION]["community_cards"]
-        try:
+    
             if (hole_cards[VERSION][0]["suit"] == hole_cards[VERSION][1]["suit"]) or (hole_cards[VERSION][0]["rank"] == hole_cards[VERSION][1]["rank"]):
                 return 25
             else: 
                 return 10
         except BaseException:
             print(community_cards)
+            return 
 
         finally: 
             return 20
