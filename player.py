5b4e6c1dc72087fe28f3af109f9e30189415b9c2
simofanna <simofanna93@gmail.com>
2018-02-19 10:48:08 +0100

    try

--- player.py
+++ player.py
@@ -2,8 +2,19 @@
 class Player:
     VERSION = "0.8"
 
-    def betRequest(self, game_state):      
-        return 20
+    def betRequest(self, game_state):    
+        holeCards = game_state["hole_cards"]
+        communityCards = game_state["community_cards"]
+
+        try:
+            if hole_cards[VERSION][0]["suit"] == hole_cards[VERSION][1]["suit"] or hole_cards[VERSION][0]["rank"] == hole_cards[VERSION][1]["rank"]:
+                return 25
+            else: 
+                return 10
+        except ValueError:
+            print(community_cards)
+    
+    return 10
 
     def showdown(self, game_state):
         pass
