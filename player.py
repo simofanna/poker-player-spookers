b6a9730fb9fe80fe257a40ea68cb90f30217d3b4
Balazs Pest <pest.balazs@gmail.com>
2018-02-19 10:07:08 +0100

    not 20

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 20
+        return 0
 
     def showdown(self, game_state):
         pass
