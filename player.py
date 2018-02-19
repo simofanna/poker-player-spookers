d7f9d4420fad9b5b1171731319632db59939f46c
simofanna <simofanna93@gmail.com>
2018-02-19 14:05:14 +0100

    ver02

--- player.py
+++ player.py
@@ -6,19 +6,26 @@ class Player:
     def betRequest(self, game_state): 
         try:
             players = game_state["players"]
-                
+            communityCards = game_state["community_cards"]
+
+           
             for player in players:
                 if player["name"] == "Spookers": 
                     ourHoleCards = player["hole_cards"]
 
                     
             if ourHoleCards[0]["rank"] == ourHoleCards[1]["rank"]:
-                return 10000
-            else:
-                return 0
+                for card in community_cards:
+                    if (card["rank"] == ourHoleCards[0]["rank"]) or (card["rank"] == ourHoleCards[1]["rank"]):
+                        return 10000
+                    else:
+                        return 100
+            return randint(80,1000)
+             
 
         except BaseException:
             print("Error rased , playes: " + players + "our hole cards: " + ourHoleCards)
+
         finally:
             return randint(80,1000)
 
