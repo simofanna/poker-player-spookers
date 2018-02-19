34f7272e8f1c29129f20446d3a4e31c0d552c702
Balazs Pest <pest.balazs@gmail.com>
2018-02-19 11:50:35 +0100

    random return

--- player.py
+++ player.py
@@ -1,9 +1,10 @@
+from random import randint
 
 class Player:
     VERSION = "8"
 
     def betRequest(self, game_state): 
-        return 1000
+        return randint(50, 1000)
 
     def showdown(self, game_state):
         pass
